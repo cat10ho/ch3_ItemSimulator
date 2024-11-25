3ec1e0b6660e4a69b8822cbc4aa66a921612bc25
이이삭 <dlrltn1091@gmail.com>
2024-11-25 15:30:08 +0900

    cors 설치, html 연결, 등등 해봄.

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ import ItemRouter from "./routes/item.router.js";
 import CharacterItemsRouter from "./routes/characterItems.router.js";
 import ShopRouter from "./routes/shop.router.js";
 import path from "path";
+import cors from 'cors';
 
 const app = express();
 const PORT = 3018;
 
+app.use(cors());
 app.use(express.static(path.resolve("assets")));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
