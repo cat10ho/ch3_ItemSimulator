28e4a48297c634858bd2ff2e85042be72b8d7421
이이삭 <dlrltn1091@gmail.com>
2024-12-02 17:16:24 +0900

    123
    123

--- src/app.js
+++ src/app.js
@@ -9,17 +9,31 @@ import CharacterItemsRouter from "./routes/characterItems.router.js";
 import ShopRouter from "./routes/shop.router.js";
 import path from "path";
 import cors from 'cors';
+import { fileURLToPath } from "url";
 
+const __dirname = fileURLToPath(new URL(".", import.meta.url));  
+const __filename = fileURLToPath(import.meta.url); 
+
+console.log(`__dirname     ->`, __dirname);
+console.log(`__filename    ->`, __filename);
+console.log(`process.cwd() ->`, process.cwd());
 
 const app = express();
 const PORT = 3018;
+
+const publicPath = path.join(process.cwd(), 'assets');
+
+console.log(publicPath);
+app.use(express.static(publicPath)); //경로의 파일을 사용할수 있게 만들어줌.
+//express static file serving
+//express 스태틱파일 서빙 
 
 app.use(cors());
 app.use(express.static(path.resolve("assets")));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.get("/",(req,res)=>{return res.send("안녕하세요.")});
+app.get("/",(req,res)=>{return res.sendFile(path.join(publicPath , "login.html"))});
 app.use("/api", [
   AccountsRouter,
   CharactersRouter,
